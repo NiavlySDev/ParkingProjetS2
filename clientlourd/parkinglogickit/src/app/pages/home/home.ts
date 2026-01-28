87fa9a0bebae85e4d3546163d785e51a7ab47318
Ethan Chandebois <github.husband810@passinbox.com>
2026-01-28 10:35:39 +0100

    update angular bel IHM

--- clientlourd/parkinglogickit/src/app/pages/home/home.ts
+++ clientlourd/parkinglogickit/src/app/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Button } from 'primeng/button';
+import { PrimengModule } from '../../shared/primeng.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Button, PrimengModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
